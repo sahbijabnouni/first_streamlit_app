286103288494ce0b8d79dfdea265e4e2128644fc
sahbijabnouni <sahbi.jabnouni@gmail.com>
2024-01-12 09:00:26 -0500

    Update streamlit_app.py
    fruits_to_show

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,5 +16,5 @@ fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.in
 
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-streamlit.dataframe(fruits_selected)
+streamlit.dataframe(fruits_to_show)
 
